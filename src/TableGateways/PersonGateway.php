bc6e8e7cae490923a31032fd51ba1042c90c5f43
Ubuntu <faccincani.al@gmail.com>
2023-01-25 16:43:34 +0000

    My first File

---
+++ src/TableGateways/PersonGateway.php
@@ -0,0 +1,113 @@
+<?php
+namespace Src\TableGateways;
+
+class PersonGateway {
+
+    private $db = null;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function findAll()
+    {
+        $statement = "
+            SELECT 
+                id, firstname, lastname, ip
+	    FROM
+               person;
+        ";
+
+        try {
+            $statement = $this->db->query($statement);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function find($id)
+    {
+        $statement = "
+            SELECT
+                id, firstname, lastname, ip
+            FROM
+		person
+          WHERE id = ?;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array($id));
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function insert(Array $input)
+    {
+        $statement = "
+            INSERT INTO person
+                (firstname, lastname, ip)
+            VALUES
+                (:firstname, :lastname, :ip);
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array(
+                'firstname' => $input['firstname'],
+                'lastname'  => $input['lastname'],
+                'ip' => $input['ip'] ?? null,
+            ));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function update($id, Array $input)
+    {
+        $statement = "
+            UPDATE person
+            SET
+                firstname = :firstname,
+                lastname  = :lastname,
+                ip  = :ip,
+            WHERE id = :id;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array(
+                'id' => (int) $id,
+                'firstname' => $input['firstname'],
+                'lastname'  => $input['lastname'],
+                'ip' => $input['ip'],
+            ));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function delete($id)
+    {
+        $statement = "
+            DELETE FROM person
+            WHERE id = :id;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array('id' => $id));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+}
